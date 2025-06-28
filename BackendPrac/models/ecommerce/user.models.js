bd515f677ae9d7397ccbe03c7a987412ad7bcf0a
IrfanSaeednarejo <irfansaeed.narejo@gmail.com>
2025-06-29 01:26:58 +0500

    ecommerce Model added

---
+++ BackendPrac/models/ecommerce/user.models.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+// Define the user schema
+ const userSchema = new mongoose.Schema({
+
+       username:{
+        type:String,
+        required:true,
+        unique:true
+       },
+       email:{
+        type:String,
+        required:true,
+        unique:true
+       },
+       password:{
+        type:String,
+        required:true
+       },
+       
+
+
+ },{timestamps:true});
+
+
+
+//User will be saved as Users in the database
+ export const User= mongoose.model("User",userSchema)
